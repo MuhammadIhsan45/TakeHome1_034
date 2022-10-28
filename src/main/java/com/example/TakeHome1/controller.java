5579cf35e1e6e1c5fcd4562fd731fcfd203af456
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-10-28 16:58:57 +0700

    commite pertama

---
+++ src/main/java/com/example/TakeHome1/controller.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.TakeHome1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author hp
+ */
+@Controller
+public class controller {
+    @RequestMapping("/nice")
+    @ResponseBody
+    public String sip(){
+        
+        
+        
+        
+    }
+   
+    
+}
---
+++ src/test/java/com/example/TakeHome1/TakeHome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TakeHome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
